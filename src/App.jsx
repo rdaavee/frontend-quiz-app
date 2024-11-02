f12c79b04fd02c7f5ea5e50e91a0b31427db47b7
rdaavee <rdavee0412@gmail.com>
2024-11-02 11:36:38 +0800

    initial commit with basic setup files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-bold text-appPurple">Frontend Quiz</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+      colors: {
+        appPurple: "#A729F5",
+        darkNavy: "#313E51",
+        navy: "#3B4D66",
+        greyNavy: "#626C7F",
+        lightBluish: "#ABC1E1",
+        lightGrey: "#F4F6FA",
+        appGreen: "#26D782",
+        appRed: "#EE5454",
+      },
+    },
+  },
+  plugins: [],
+};
