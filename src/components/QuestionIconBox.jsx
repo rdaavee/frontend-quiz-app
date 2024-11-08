3fcb67bee30c1821ff16cf9f229d5948d926eb2d
rdaavee <rdavee0412@gmail.com>
2024-11-08 15:34:11 +0800

    chore: resize QuestionIconBox to 12x12

--- src/components/QuestionIconBox.jsx
+++ src/components/QuestionIconBox.jsx
@@ -2,7 +2,7 @@ const QuestionIconBox = ({ title, icon, backgroundCode }) => {
 
     return (
         <div
-            className="rounded-lg h-14 w-14 flex items-center justify-center text-center p-2"
+            className="rounded-lg h-12 w-12 flex items-center justify-center text-center p-2"
             style={{ backgroundColor: backgroundCode }}>
             <img
                 className="w-full h-full object-contain"
